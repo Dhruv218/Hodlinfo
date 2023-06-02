ae55c7620ecca1bb55e855d08c614baa074ae84e
Dhruv Aggarwal <adhruv218@gmail.com>
2023-06-02 20:03:37 +0530

    first commit

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/currency
-    axios.get('http://localhost:8000/api/currency')
+    axios.get('/api/currency')
         .then(function(response){
             res.render('index', { data : response.data });
         })
